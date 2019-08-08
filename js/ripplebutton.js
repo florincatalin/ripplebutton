b7ac638c48df93c442a1ee3a4ecd9a626d92b5b2
Florin Catalin <23093563+florincatalin@users.noreply.github.com>
2019-08-08 18:50:50 +0300

    Create ripplebutton.js

---
+++ js/ripplebutton.js
@@ -0,0 +1,29 @@
+var btnactiv = document.getElementById("ripbtn1");
+btnactiv.addEventListener('click', functia1);
+ function functia1(){
+alert('Hello!');
+ };
+ 
+ var btnactiv = document.getElementById("ripbtn2");
+btnactiv.addEventListener('click', functia2);
+ function functia2(){
+alert('Salut!');
+ };
+ 
+ var btnactiv = document.getElementById("ripbtn3");
+btnactiv.addEventListener('click', functia3);
+ function functia3(){
+alert('Bonjour!');
+ };
+ 
+ var btnactiv = document.getElementById("ripbtn4");
+btnactiv.addEventListener('click', functia4);
+ function functia4(){
+alert('Hallo!');
+ };
+ 
+/* btnactiv.addEventListener('mouseover', functia1);
+ function functia1(){   
+btnactiv.style.cursor ="pointer";
+ }; */
+
